9787807eee80377b0dc446497845d6517ffed1d2
Giovanni Remanente <gioremanente13@gmail.com>
2023-09-19 14:21:58 +0800

    separating map methods

---
+++ src/Map.ts
@@ -0,0 +1,16 @@
+export class Map {
+  private googleMap: google.maps.Map;
+
+  constructor() {
+    this.googleMap = new google.maps.Map(
+      document.getElementById('map') as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+}
